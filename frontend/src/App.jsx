52451aedac323e474709ac39f78fce8c546240a9
seon woo ban <bansw96.gmail.com>
2024-11-08 18:58:49 +0900

    seonwoo commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>비트 + 리액트</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        더 학습하기를 원하신다면, 비트와 리액트 로고를 클릭해주세여
       </p>
     </>
   )
